810a3916df931ca19d1a6d15b16c2e546a0485bb
nectariosouzo <nectariosouzounidis@gmail.com>
2023-09-07 19:49:06 -0400

    figuring out tests

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,15 +1,27 @@
 import * as assert from 'assert';
-
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
+import * as sinon from "sinon";
+import { main } from '../../extension';
+import GptHandler from '../../gptHandler';
 
 suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+	test('should handle user input and API response', async () => {
+		let gpt = new GptHandler('');
+		const doc = await vscode.workspace.openTextDocument({ content: 'h' });
+		const editor = await vscode.window.showTextDocument(doc);
+		console.log(editor.selection.active);
+		// Show the document in a TextEditor
+		let gptStub = sinon.stub(gpt, 'callGpt').resolves('{"regex": "test", "explanation": "Test explanation"}');
+		const showInputBoxStub = sinon.stub(vscode.window, 'showInputBox').resolves('User input');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+		await main(gpt, editor);
+
+		sinon.assert.calledOnce(showInputBoxStub);
+		sinon.assert.calledOnce(gptStub);
+		assert.equal("test", doc.getText());
+		showInputBoxStub.restore();
+		gptStub.restore();
+	  });
 });
