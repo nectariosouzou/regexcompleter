db4e4eec37ec9856043ee26a794ad54fcce75518
nectariosouzou <88638503+nectariosouzou@users.noreply.github.com>
2023-09-08 18:26:34 -0400

    Release 1.0 (#4)
    * Fix minor typos (#1)
    
    * Remove loggers (#2)
    
    * Feat/refactor (#3)
    
    * update github workflow for develop branch (#5)

---
+++ src/gptHandler.ts
@@ -0,0 +1,52 @@
+import fetch, { RequestInit } from "node-fetch";
+
+const TIMEOUT = 11000;
+
+interface GptIntereface {
+    url: string;
+    callGpt(userPrompt: string): Promise<string>;
+}
+
+ export default class GptHandler implements GptIntereface {
+    url: string;
+
+    constructor(url: string) {
+        this.url = url;
+    }
+
+    async callGpt(userPrompt: string): Promise<string> {
+        const bodyData = {
+            prompt: userPrompt
+        };
+        const bodyJson = JSON.stringify(bodyData);
+        const options: RequestInit = {
+            method: 'POST',
+            headers: {
+                // eslint-disable-next-line @typescript-eslint/naming-convention
+                'Content-Type': 'application/json',
+                // eslint-disable-next-line @typescript-eslint/naming-convention
+                'Content-Length': bodyJson.length.toString()
+            },
+            body: bodyJson
+        };
+        try {
+            const response = await Promise.race([fetch(this.url, options), createTimeoutPromise()]);
+            if (!response.ok) {
+                throw new Error(`HTTP request failed with status: ${response.status}`);
+            }
+            const responseData = await response.text();
+            return responseData;
+        } catch (error) {
+            throw error;
+        }
+    }
+}
+
+
+function createTimeoutPromise(): Promise<never> {
+    return new Promise<never>((_, reject) => {
+        setTimeout(() => {
+            reject(new Error('Request timed out'));
+        }, TIMEOUT);
+    });
+}
